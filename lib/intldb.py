18ad20b81642b0f99b34056efb73c5ad7c5f4bd7
Dhanajayan K <72462261+virtualheart@users.noreply.github.com>
2023-05-11 10:01:16 +0530

    Update intldb.py

--- lib/intldb.py
+++ lib/intldb.py
@@ -1,26 +1,37 @@
 import sqlite3
 
+# edit this
+uname="admin"
+pw="admin789"
+
 conn = sqlite3.connect("db/data.db")
 
 try:
+
     cursor = conn.execute("SELECT * from ADMIN limit 1")
+
 except Exception as e:
 
-    conn.execute("""
-        CREATE TABLE ADMIN(
-        ADMIN_ID INTEGER PRIMARY KEY AUTOINCREMENT  NOT NULL ,
-        USERNAME TEXT NOT NULL, 
-        PASSWORD TEXT NOT NULL)
-    """)
-    print("Table ADMIN created successfully")
+    try:
+        conn.execute("""
+            CREATE TABLE ADMIN(
+            ADMIN_ID INTEGER PRIMARY KEY AUTOINCREMENT  NOT NULL ,
+            USERNAME TEXT NOT NULL, 
+            PASSWORD TEXT NOT NULL)
+        """)
+        print("Table ADMIN created successfully")
+        
+        qur="INSERT INTO ADMIN(USERNAME,PASSWORD) VALUES ('" + uname + "','" + pw +"')"
 
-    conn.execute(
-        "INSERT INTO ADMIN(USERNAME,PASSWORD) VALUES ('admin', 'admin789')")
+        conn.execute(qur)
 
-    # conn.execute(
-        # "INSERT INTO ADMIN(USERNAME,PASSWORD) VALUES ('krazy', 'krazy789')")
-    print("Username : admin\nPassword : admin789")
-    conn.commit()
+        print("Username : "+ uname +"\nPassword : "+pw )
+        conn.commit()
+    
+    except Exception as e:
+        print(e)
+    finally:
+        conn.close()
 
 finally:
     conn.close()
